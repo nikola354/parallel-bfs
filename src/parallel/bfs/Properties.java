98d094314105f0acc24fc4d2bf5d99a95350fdad
Nikola Manolov <n.manolov@stenik.bg>
2024-09-11 16:13:25 +0300

    Change record to class because of old Java version

--- src/parallel/bfs/Properties.java
+++ src/parallel/bfs/Properties.java
@@ -1,4 +1,37 @@
 package parallel.bfs;
 
-public record Properties(int verticesCount, double density, boolean quiet, boolean directed, int threadsCount) {
+public class Properties {
+    private final int verticesCount;
+    private final double density;
+    private final boolean quiet;
+    private final boolean directed;
+    private final int threadsCount;
+
+    public Properties(int verticesCount, double density, boolean quiet, boolean directed, int threadsCount) {
+        this.verticesCount = verticesCount;
+        this.density = density;
+        this.quiet = quiet;
+        this.directed = directed;
+        this.threadsCount = threadsCount;
+    }
+
+    public int verticesCount() {
+        return verticesCount;
+    }
+
+    public double density() {
+        return density;
+    }
+
+    public boolean quiet() {
+        return quiet;
+    }
+
+    public boolean directed() {
+        return directed;
+    }
+
+    public int threadsCount() {
+        return threadsCount;
+    }
 }
